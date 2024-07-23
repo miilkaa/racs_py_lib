9aa77efed8f8f1f88510bd705c6c366cb9a22544
miilkaa <evgeny.milyukov@crmgroup.ru>
2024-07-23 10:14:17 +0300

    fix name of parameter

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="racs-py-lib",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "requests"
